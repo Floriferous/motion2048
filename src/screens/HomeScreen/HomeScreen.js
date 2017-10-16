b8b0862adb4ab4378480d263f51aacd14242c8ea
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-16 21:09:42 +0200

    added disclaimer

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -33,14 +33,25 @@ const styles = {
     borderRadius: '4px',
     cursor: 'pointer',
   },
+  link: {
+    textDecoration: 'none',
+    color: colors.white,
+    opacity: 0.4,
+  },
 };
 
 const HomeScreen = ({ onGameStart, highscore, onlineHighscores }) => (
   <div style={styles.div}>
     <KeyboardController onEnter={onGameStart} />
-    <h1 className="animated fadeInLeft" style={styles.title}>
+    <h1 className="animated fadeInTop" style={styles.title}>
       Motion 2048
     </h1>
+    <h5 style={{ margin: 0, marginBottom: 16 }} className="animated fadeInLeft">
+      A shameless and total ripoff of the awesome{' '}
+      <a href="https://gabrielecirulli.github.io/2048/" style={styles.link}>
+        2048
+      </a>
+    </h5>
     <h4 className="animated fadeInRight" style={styles.subtitle}>
       by Florian Bienefelt
     </h4>
