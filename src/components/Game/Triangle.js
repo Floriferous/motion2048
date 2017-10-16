8572b6bfd7c858b4781ac9e9551fc93bc2821d37
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-16 15:55:54 +0200

    added color animations when clicking the game

--- src/components/Game/Triangle.js
+++ src/components/Game/Triangle.js
@@ -1,46 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const getStyles = side => {
-  let position;
-  switch (side) {
-    case 'UP':
-      position = { bottom: '50%', right: '50%' };
-      break;
-    case 'RIGHT':
-      position = { bottom: '50%', left: '50%' };
-      break;
-    case 'DOWN':
-      position = { top: '50%', left: '50%' };
-      break;
-    case 'LEFT':
-      position = { top: '50%', right: '50%' };
-      break;
-    default:
-      throw new Error(`invalid side: ${side}`);
+export default class Triangle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { backgroundColor: 'transparent' };
   }
 
-  return {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    ...position,
-  };
-};
+  getStyles = () => {
+    const { side } = this.props;
+    const { backgroundColor } = this.state;
+    let position;
+    switch (side) {
+      case 'UP':
+        position = { bottom: '50%', right: '50%' };
+        break;
+      case 'RIGHT':
+        position = { bottom: '50%', left: '50%' };
+        break;
+      case 'DOWN':
+        position = { top: '50%', left: '50%' };
+        break;
+      case 'LEFT':
+        position = { top: '50%', right: '50%' };
+        break;
+      default:
+        throw new Error(`invalid side: ${side}`);
+    }
 
-const Triangle = ({ side, onClickTriangle }) => {
-  return (
-    <div
-      className={side}
-      style={getStyles(side)}
-      onClick={() => onClickTriangle(side)}
-    />
-  );
-};
+    return {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      transition: 'background-color 100ms ease',
+      backgroundColor,
+      ...position,
+    };
+  };
+
+  handleClick = () => {
+    const { side, onClickTriangle } = this.props;
+    onClickTriangle(side);
+    this.setState({ backgroundColor: 'rgba(200,200,200, 0.2)' }, () => {
+      setTimeout(() => {
+        this.setState({ backgroundColor: 'transparent' });
+      }, 100);
+    });
+  };
+
+  render() {
+    const { side } = this.props;
+    return (
+      <div
+        className={side}
+        style={this.getStyles()}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
 
 Triangle.propTypes = {
   side: PropTypes.string.isRequired,
   onClickTriangle: PropTypes.func.isRequired,
 };
-
-export default Triangle;
