61152dc154650bf3b8e10943d1498e632190d930
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-16 17:50:55 +0200

    fixed linting warnings

--- src/js/getNewBoxDetails.js
+++ src/js/getNewBoxDetails.js
@@ -6,7 +6,7 @@ import groupByAxis from './groupByAxis';
 const getUid = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     const r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
+      v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 };
